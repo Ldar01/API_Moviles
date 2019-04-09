cc629a2beca7cc04e03ecde970cb31ae3c83879c
Luisa Arevalo <00038617@uca.edu.sv>
2019-04-09 14:56:58 -0600

    Agregando ruta coins

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 mongoose.Promise = global.Promise;
-mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/nombredebase").then(function(){
   console.log("You are connected :D");
 }).catch(function(err){
--- routes/coins.js
+++ routes/coins.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var coinsController = require('../controllers/coinsController')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', coinsController.getAll);
+router.get('/:id', coinsController.getOne);
+router.post('/',coinsController.Create);
+router.put('/:id',coinsController.Update);
+router.delete('/:id',coinsController.Delete);
 
 module.exports = router;
